360d7929f5c70c77eedafbaf407688aebbc5eab0
Fadlul Alim <fad.lee@hotmail.com>
2019-10-29 12:46:46 +0700

    Update custom-upload-folder.php

--- custom-upload-folder.php
+++ custom-upload-folder.php
@@ -16,7 +16,7 @@ class CustomUploadFolder
 			setcookie( 'custom_upload_folder', '' );
 		}
 
-		add_action( 'pre-upload-ui', 'custom_upload_folder_select' );
+		add_action( 'pre-upload-ui', [$this, 'custom_upload_folder_select'] );
 
 		// Media table
 		add_filter( 'manage_media_columns', function( $columns ) {
@@ -111,19 +111,17 @@ class CustomUploadFolder
 			</form>
 		</div>
 	<?php }
+
+	function custom_upload_folder_select() {
+		$folders = explode( "\n", get_option( 'custom_upload_folders', "assets\r\nassets/img\r\nassets/css\r\nassets/js" ) ); ?>
+		<?php _e( 'Select Upload Folder', __FILE__ ); ?>
+		<select class="js-custom-upload-folder" onchange="document.cookie = 'custom_upload_folder=' + event.target.value;">
+			<option value=""><?php _e( 'Choose Folder', __FILE__ ); ?></option>
+			<?php foreach ( $folders as $folder ) {
+				echo '<option value="' . $folder . '">' . $folder . '</option>';
+			} ?>
+		</select>
+	<?php }
 }
 
 new CustomUploadFolder;
-
-function custom_upload_folder_select() {
-	$folders = explode( "\n", get_option( 'custom_upload_folders', "assets\r\nassets/img\r\nassets/css\r\nassets/js" ) );
-	?>
-	<?php _e( 'Select Upload Folder', __FILE__ ); ?>
-	<select class="js-custom-upload-folder" onchange="document.cookie = 'custom_upload_folder=' + event.target.value;">
-		<option value=""><?php _e( 'Choose Folder', __FILE__ ); ?></option>
-		<?php foreach ( $folders as $folder ) {
-			echo '<option value="' . $folder . '">' . $folder . '</option>';
-		} ?>
-	</select>
-	<?php
-}
